b353ba3ad32a9aa77c9694b9bce9734b62e0db01
Juergen Fesslmeier <jfesslmeier@gmail.com>
2014-03-07 16:26:40 -0800

    Overriding user parameter sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,4 +50,12 @@ class ApplicationController < ActionController::Base
   def cookie_auth_token
     "#{session_param}_auth_token".to_sym
   end
+  
+  def devise_parameter_sanitizer
+    if resource_class == User
+      User::ParameterSanitizer.new(User, :user, params)
+    else
+      super
+    end
+  end
 end
