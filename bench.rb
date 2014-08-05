1b2d6f15c46c582ea2a7145d154f4e12648d769f
Juergen Fesslmeier <juergen@schnappsidea.local>
2014-08-05 09:57:59 -0700

    Moved bench.rb to test

--- bench.rb
+++
@@ -1,9 +0,0 @@
-require 'benchmark'
-require 'active_support/core_ext/array'
-
-n = 500000
-
-Benchmark.bmbm do |x|
-  x.report("[].flatten") { n.times { [["a"]].flatten } }
-  x.report("Array.wrap") { n.times { Array.wrap(["a"]) } }
-end
