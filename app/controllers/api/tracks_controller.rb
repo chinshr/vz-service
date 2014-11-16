10dde389d2bcf4659d9cf6bbe89452f3d821bdeb
Juergen Fesslmeier <jfesslmeier@gmail.com>
2014-11-16 13:57:14 -0800

    Remove trailing space

--- app/controllers/api/tracks_controller.rb
+++ app/controllers/api/tracks_controller.rb
@@ -4,9 +4,9 @@ class Api::TracksController < Api::ApplicationController
   def show
     @document.track
   end
-  
+
   protected
-  
+
   def load_document
     @document = Document.find(params[:document_id])
   end
