d2e661684745e654d01b4fa23c5732a65632363a
Juergen Fesslmeier <jfesslmeier@gmail.com>
2014-11-20 09:30:28 -0800

    Initializing editor

--- app/assets/javascripts/views/documents/edit.js
+++ app/assets/javascripts/views/documents/edit.js
@@ -32,7 +32,7 @@ App.Views.DocumentsEdit = Backbone.View.extend({
 
     if (this.model.ok) {
       $('#document-loading').hide();
-      this.editor();
+      this.initializeEditor();
       $('#document-edit').show();
     } else if (this.model.errors) {
       $('#document-load-error').show();
@@ -41,7 +41,7 @@ App.Views.DocumentsEdit = Backbone.View.extend({
     return this;
   },
 
-  editor: function() {
+  initializeEditor: function() {
     this.titleEditor = new Quill('#title-editor', {
       'modules': {
       },
@@ -61,50 +61,53 @@ App.Views.DocumentsEdit = Backbone.View.extend({
     this.contentEditor.onModuleLoad('toolbar', function(toolbar) {
       $('#content-editor iframe').contents().find('body').css('overflow', 'hidden');
     });
-    return;
 
-    var moveUserInitials = function() {
-      var sel = contentEditor.root.ownerDocument.getSelection();
-      if (sel && sel.rangeCount > 0) {
-        var selrg = sel.getRangeAt(0);
-        if (selrg) {
-          var rects = selrg.getClientRects();
-          if (rects.length > 0) {
-            var ui = $(".user-initials");
-            ui.stop().animate({
-              top: 100 - (ui.height() / 2) + rects[0].top
-            }, 50);
+    this.contentEditor.on('text-change', (function(_this) {
+      return function(delta, source) {
+        // expand window
+        $('#content-editor').height(_this.contentEditor.root.ownerDocument.body.scrollHeight);
+        _this.moveUserInitials();
+      }
+    })(this));
+
+    this.contentEditor.on('selection-change', (function(_this) {
+      return function(range) {
+        if (range) {
+          if (range.start == range.end) {
+            // console.log('User cursor is on', range.start);
+            _this.moveUserInitials();
+          } else {
+            // var text = editor.getText(range.start, range.end);
+            // console.log('User has highlighted', text);
           }
-        }
-      }
-    }
-
-    contentEditor.on('text-change', function(delta, source) {
-      // expand window
-      $('#content-editor').height(contentEditor.root.ownerDocument.body.scrollHeight);
-
-      moveUserInitials();
-    });
-
-    contentEditor.on('selection-change', function(range) {
-      if (range) {
-        if (range.start == range.end) {
-          // console.log('User cursor is on', range.start);
-          moveUserInitials();
         } else {
-          // var text = editor.getText(range.start, range.end);
-          // console.log('User has highlighted', text);
+          // console.log('Cursor not in the editor');
         }
-      } else {
-        // console.log('Cursor not in the editor');
       }
-    });
+    })(this));
 
-    var keyboard = contentEditor.getModule('keyboard');
+    var keyboard = this.contentEditor.getModule('keyboard');
     keyboard.addHotkey({key: 32, metaKey: true, shiftKey: true}, function(range) {
       console.log('user hit Shift+Cmd+Space');
       return true;   // return false will prevent other listeners from receiving the event
     });
 
+  },
+
+  moveUserInitials: function() {
+    var sel = this.contentEditor.root.ownerDocument.getSelection();
+    if (sel && sel.rangeCount > 0) {
+      var selrg = sel.getRangeAt(0);
+      if (selrg) {
+        var rects = selrg.getClientRects();
+        if (rects.length > 0) {
+          var ui = $(".user-initials");
+          ui.stop().animate({
+            top: 100 - (ui.height() / 2) + rects[0].top
+          }, 50);
+        }
+      }
+    }
   }
+
 });
