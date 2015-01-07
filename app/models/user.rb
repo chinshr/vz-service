a7a472720b6d545722baece387f27367b203a962
Juergen Fesslmeier <jfesslmeier@gmail.com>
2015-01-07 15:05:23 -0800

    Removing trailing spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :trackable, :validatable, 
+    :recoverable, :rememberable, :trackable, :validatable,
     :confirmable
 
   geocoded_by :ip_address, :latitude  => :lat, :longitude => :lng
@@ -16,18 +16,18 @@ class User < ActiveRecord::Base
       record.country_code = geo.country_code
     end
   end
-  
+
   has_many :documents, dependent: :nullify
   has_many :ingests, through: :documents
   has_many :uploads, through: :ingests
-  
+
   acts_as_tagger
-  
+
   validates :first_name, presence: true, :if => :confirmed_or_confirmation_validation?
   validates :last_name, presence: true, :if => :confirmed_or_confirmation_validation?
-  
+
   scope :confirmed, lambda {where("users.confirmed_at IS NOT NULL")}
-  
+
   before_save :geocode, :if => :has_ip_address?, :unless => :geocoded?
   before_save :reverse_geocode, :if => :geocoded?
 
@@ -41,11 +41,11 @@ class User < ActiveRecord::Base
     # unless_confirmed {yield}
     pending_any_confirmation {yield}
   end
-  
+
   def has_no_password?
     encrypted_password.blank?
   end
-  
+
   def confirm_set_with(params)
     @confirmation_validation = true
     update_attributes(params)
@@ -54,7 +54,7 @@ class User < ActiveRecord::Base
   def confirmation_validation?
     !!@confirmation_validation
   end
-  
+
   def confirmed_or_confirmation_validation?
     confirmed? || confirmation_validation?
   end
@@ -64,7 +64,7 @@ class User < ActiveRecord::Base
   def has_ip_address?
     ip_address.present?
   end
-  
+
   def ip_address
     current_sign_in_ip || last_sign_in_ip
   end
