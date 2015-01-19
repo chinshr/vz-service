469cbe678ff9b4837d24b7652a3d6da57383a1b1
Juergen Fesslmeier <jfesslmeier@gmail.com>
2015-01-19 09:35:17 -0800

    Fixed beachstrap sprockets

--- app/assets/javascripts/beachstrap.js
+++ app/assets/javascripts/beachstrap.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.ui.core
-//= require jquery.ui.widget
+//= require jquery-ui/core
+//= require jquery-ui/widget
 //= require lib/bootstrap
 //= require lib/beachstrap/waypoints.min.js
 //= require lib/beachstrap/beachstrap-script
