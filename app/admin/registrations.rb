9f366dd323129161bd5f066eb1af5b387cd2621c
Juergen Fesslmeier <jfesslmeier@gmail.com>
2015-02-10 19:34:00 -0800

    Registration active admin resource

---
+++ app/admin/registrations.rb
@@ -0,0 +1,31 @@
+ActiveAdmin.register Registration do
+
+  scope :all
+  scope :recent
+
+  index do
+    column :email
+    column :name
+    column :locale
+    column :ip_address
+    column :created_at
+    # default_actions
+    column do |resource|
+      links = link_to I18n.t('active_admin.view'), resource_path(resource), :class => "member_link view_link"
+      links += link_to I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link"
+      links += link_to I18n.t('active_admin.delete'), resource_path(resource), 
+        :method => :delete, :class => "member_link delete_link", :confirm => "Are you really really really sure?"
+      (resource.aasm.events(resource.aasm.current_state) - []).each do |event|
+        links += link_to event.to_s.humanize, switch_admin_registration_path(resource, params.merge(:event => event)), 
+          :class => "member_link view_link button", :confirm => "Really want to #{event.to_s.humanize.downcase}?"
+      end
+      links
+    end
+  end
+
+  member_action :switch do
+    resource = Registration.find(params[:id])
+    resource.send(:"#{params[:event]}!")
+    redirect_to admin_registrations_path
+  end
+end
