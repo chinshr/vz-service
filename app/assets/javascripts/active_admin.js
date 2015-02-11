f9cdfbbbda4861561b91bfb58219a6f34907f8e5
Juergen Fesslmeier <jfesslmeier@gmail.com>
2015-02-10 19:34:49 -0800

    Added confirm function

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,1 +1,11 @@
 //= require active_admin/base
+
+$(function() {
+  $('.member_link[confirm]').on('click', function(e) {
+    if (confirm($(this).attr('confirm'))) {
+      // execute
+    } else {
+      e.preventDefault();
+    }
+  });
+});
