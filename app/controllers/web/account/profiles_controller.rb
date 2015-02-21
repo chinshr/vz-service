ade7d42b7086fda4f36693c32a5ec2df6de00634
Juergen Fesslmeier <jfesslmeier@gmail.com>
2015-02-21 15:45:26 -0800

    Account billing and profile

--- app/controllers/web/account/profiles_controller.rb
+++ app/controllers/web/account/profiles_controller.rb
@@ -1,2 +1,15 @@
 class Web::Account::ProfilesController < Web::Account::ApplicationController
+
+  def update
+    @user = current_user
+    @user = User.update(@user.id, account_profile_params)
+    respond_with @user, :location => web_account_profile_path
+  end
+
+protected
+
+  def account_profile_params
+    params.require(:user).permit(:first_name, :last_name, :time_zone)
+  end
+
 end
