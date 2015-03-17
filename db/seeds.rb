790bdbde34dbcd2046e41e5c17ee670a2d02e706
Juergen Fesslmeier <jfesslmeier@gmail.com>
2015-03-16 20:54:42 -0700

    Updating password change dialog

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.find_or_create_by(email: 'manager@voyz.es') do |u|
-  u.password              = 'paloalto605:voyzes' 
+  u.password              = 'paloalto605:voyzes'
   u.password_confirmation = 'paloalto605:voyzes'
 end
