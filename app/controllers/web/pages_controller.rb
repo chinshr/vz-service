10fe91d63eab6b8b6091cd1f0668d65ec90d56fd
Juergen Fesslmeier <jfesslmeier@gmail.com>
2015-03-20 22:20:58 -0700

    Last modified set header

--- app/controllers/web/pages_controller.rb
+++ app/controllers/web/pages_controller.rb
@@ -1,6 +1,15 @@
 class Web::PagesController < Web::ApplicationController
+  before_action :set_cache_headers
+
   def index
-    expires_in rack_cache_time, public: true
     render :layout => "beachstrap"
+  end
+
+  protected
+
+  def set_cache_headers
+    last_modified = File.mtime("#{Rails.root}/app/views/web/pages/#{action_name}.html.erb")
+    fresh_when last_modified: last_modified , public: true, etag: last_modified
+    expires_in rack_cache_time, public: true
   end
 end
