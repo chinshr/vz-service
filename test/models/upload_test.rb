3b57e35db497706132c0abf5e7e2a4b27fe3c02e
Juergen Fesslmeier <jfesslmeier@gmail.com>
2015-04-20 16:01:07 -0700

    Whitespace remove

--- test/models/upload_test.rb
+++ test/models/upload_test.rb
@@ -4,7 +4,7 @@ class UploadTest < ActiveSupport::TestCase
   context "associations" do
     should have_one(:ingest)
   end
-  
+
   context "validations" do
     should validate_presence_of :file_name
     should ensure_length_of(:file_name).is_at_most(255)
@@ -12,9 +12,9 @@ class UploadTest < ActiveSupport::TestCase
     should ensure_length_of(:file_type).is_at_most(255)
     should validate_presence_of :s3_url
     should ensure_length_of(:s3_url).is_at_most(255)
-    
+
     should "validate presence of title on update" do
-      upload = Upload.create(type: "audio", file_name: "audio-test.m4a", file_type: "audio/x-m4a", 
+      upload = Upload.create(type: "audio", file_name: "audio-test.m4a", file_type: "audio/x-m4a",
         file_size: 12345, s3_url: "http://s3.amazonaws.com/dropbox/audio-test.m4a")
       assert_equal upload.humanized_file_name, upload.title
       upload.title = ""
@@ -22,10 +22,10 @@ class UploadTest < ActiveSupport::TestCase
       assert_equal ["can't be blank"], upload.errors[:title]
     end
   end
-  
+
   context "callbacks" do
     should "before_validation :set_title, on: :create" do
-      upload = Upload.new(type: "audio", file_name: "audio.m4a", file_type: "audio/x-m4a", 
+      upload = Upload.new(type: "audio", file_name: "audio.m4a", file_type: "audio/x-m4a",
         file_size: 12345, s3_url: "http://s3.amazonaws.com/dropbox/audio.m4a")
       assert_equal upload.humanized_file_name, upload.title
       assert_equal true, upload.save
@@ -38,12 +38,12 @@ class UploadTest < ActiveSupport::TestCase
     setup do
       @upload = FactoryGirl.create(:upload_audio)
     end
-    
+
     should "have filtered scopes" do
-      assert_equal [:any_of_status, :none_of_status, :sort_order, :reverse_sort, :offset, :limit].to_set, 
+      assert_equal [:any_of_status, :none_of_status, :sort_order, :reverse_sort, :offset, :limit].to_set,
         Upload.scopes.to_set
     end
-    
+
     should "have any_of_status" do
       @upload.ingest.update_attribute(:aasm_state, "started")
       assert_equal [@upload], Upload.any_of_status([1, 2])
@@ -54,13 +54,13 @@ class UploadTest < ActiveSupport::TestCase
       assert_equal [@upload], Upload.none_of_status([0, 1, 3, 4, 5, 6, 7, 8, 9, 10])
     end
   end # context "scopes"
-  
+
   context "delegate" do
     setup do
-      @upload = Upload.create(type: "audio", file_name: "audio.m4a", file_type: "audio/x-m4a", 
+      @upload = Upload.create(type: "audio", file_name: "audio.m4a", file_type: "audio/x-m4a",
         file_size: 12345, s3_url: "http://s3.amazonaws.com/dropbox/audio.m4a")
     end
-    
+
     should delegate :user, to: :ingest
     should delegate :user=, to: :ingest
     should delegate :privacy, to: :ingest, allow_nil: true
@@ -89,12 +89,12 @@ class UploadTest < ActiveSupport::TestCase
       @upload = Upload.find_by_id(@upload.id)
       assert_equal user, @upload.user
     end
-    
+
     should "delegate :privacy" do
       assert_equal ["public"], @upload.privacy
       assert_equal @upload.ingest.ingestable.privacy, @upload.privacy
     end
-    
+
     should "delegate :status" do
       assert_equal @upload.ingest.status, @upload.status
     end
@@ -143,28 +143,28 @@ class UploadTest < ActiveSupport::TestCase
       assert_equal "it-IT", @upload.ingest.ingestable.locale
     end
   end # context "delegate"
-  
+
   should "humanize file name" do
     assert_equal "I like pickles", Upload::Audio.new(file_name: "i_like_pickles.m4a").humanized_file_name
     assert_equal "I like pickles", Upload::Audio.new(file_name: "i-like-pickles.m4a").humanized_file_name
   end
-  
+
   should "have s3_key" do
     upload = FactoryGirl.create(:upload_audio, :s3_url => "http://s3.amazonaws.com/dropbox/61glI7mwmN")
     assert_equal "61glI7mwmN", upload.s3_key
   end
-  
+
   should "generate object name" do
     assert_equal 10, Upload.generate_object_name.length
   end
-  
+
   should "tell if locale has recently changed" do
     upload = FactoryGirl.create(:upload_audio, :s3_url => "http://s3.amazonaws.com/dropbox/61glI7mwmN")
     assert_equal false, upload.has_locale_recently_changed?
     upload.locale = "de-DE"
     assert_equal true, upload.has_locale_recently_changed?
   end
-  
+
   should "destroy" do
     upload = FactoryGirl.create(:upload_audio, :s3_url => "http://s3.amazonaws.com/dropbox/61glI7mwmN")
     assert upload.ingest, "should have an ingest"
@@ -175,5 +175,5 @@ class UploadTest < ActiveSupport::TestCase
       assert_equal :removing, ingest.state
     end
   end
-  
+
 end
