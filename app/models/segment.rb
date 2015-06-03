4b9a92a67882d865c3e6e8ef8bdf2f708a5fd468
Juergen Fesslmeier <jfesslmeier@gmail.com>
2015-06-03 14:52:58 -0700

    Destroy track if last of segment

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -3,4 +3,12 @@ class Segment < ActiveRecord::Base
   belongs_to :chunk
   belongs_to :track
   belongs_to :ingest
+
+  after_commit :destroy_track, on: :destroy
+
+  protected
+
+  def destroy_track
+    track.destroy if track_id && Segment.where(track_id: track_id).count == 0
+  end
 end
--- test/models/segment_test.rb
+++ test/models/segment_test.rb
@@ -7,4 +7,25 @@ class SegmentTest < ActiveSupport::TestCase
     should belong_to :ingest
     should belong_to :chunk
   end
+
+  context "destroy" do
+    should "if last track" do
+      segment = FactoryGirl.create(:segment)
+      assert_difference "Segment.count", -1 do
+        assert_difference "Track.count", -1 do
+          segment.destroy
+        end
+      end
+    end
+
+    should "not destroy track if other segments with same track" do
+      segment1 = FactoryGirl.create(:segment)
+      segment2 = FactoryGirl.create(:segment, track: segment1.track)
+      assert_difference "Segment.count", -1 do
+        assert_no_difference "Track.count" do
+          segment2.destroy
+        end
+      end
+    end
+  end
 end
