883e1d1cdf4f6ca759f4b93cb706d32d97eccd7c
Juergen Fesslmeier <jfesslmeier@gmail.com>
2015-06-11 10:50:32 -0700

    Update benchmark

--- test/bench.rb
+++ test/bench.rb
@@ -1,9 +1,14 @@
 require 'benchmark'
 require 'active_support/core_ext/array'
 
-n = 500000
+n = 5000
+
+arr = []
+n.times {|i| arr << [i]}
+
 
 Benchmark.bmbm do |x|
-  x.report("[].flatten") { n.times { [["a"]].flatten } }
-  x.report("Array.wrap") { n.times { Array.wrap(["a"]) } }
+  x.report("[].flatten") { n.times { [arr].flatten } }
+  x.report("Array.wrap") { n.times { Array.wrap(arr) } }
+  x.report("[*e]") { n.times { [*arr] } }
 end
