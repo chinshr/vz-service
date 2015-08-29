d40fd221224adf660d25dc5f00fd69438f8280bb
Juergen Fesslmeier <jfesslmeier@gmail.com>
2015-08-28 18:29:15 -0700

    Enabling tags for publish view

--- app/assets/javascripts/views/documents/edit.js
+++ app/assets/javascripts/views/documents/edit.js
@@ -30,63 +30,6 @@ App.Views.DocumentsEdit = App.Views.DocumentsBase.extend({
     return this;
   },
 
-  initTagEditor: function() {
-    $('.input-taggable').select2({
-      minimumInputLength: 3,
-      multiple: true,
-      maximumInputLength: 15,
-      tokenSeparators: [",", " ", ".", "|"],
-      ajax: {
-        url: window.location.protocol + "//" + window.location.host + "/api/tags.json",
-        dataType: 'json',
-        type: 'GET',
-        quietMillis: 100,
-        data: function (term, page) { // page is the one-based page number tracked by Select2
-          return {
-            named_like: term, // search term
-            most_used: 10, // page size
-            offset: (page - 1) * 10 // page number
-          };
-        },
-        results: function (data, page) {
-          var results = [];
-          $.each(data.tags, function(index, item){
-            results.push({
-              id: item.id,
-              text: item.name
-            });
-          });
-          return {results: results, more: false};
-        }
-      },
-
-      initSelection: function(el, callback) {
-        var data = [];
-        $(el.val().split(',')).each(function() {
-          data.push({id: this, text: this});
-        });
-        callback(data);
-      },
-
-      createSearchChoice: function (term, data) {
-        if ($(data).filter( function() {
-          return this.text.localeCompare(term) === 0;
-        }).length === 0) {
-          return {id:term, text:term};
-        }
-      }
-    }).on('change', (function(_this) {
-      return function(event) {
-        var tag_list = $(event.target).val();
-        tag_list = tag_list.split(',');
-        // console.log('tags current: ', _this.model.attributes);
-        // console.log('tags new: ', tag_list);
-        _this.model.set({tag_list: tag_list});
-        // _this.saving();
-      }
-    })(this));
-  },
-
   isEdit: function() { return true; }
 
 });
