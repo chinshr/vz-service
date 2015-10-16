6df248e32ca3e8bb973758acfed4ef7f76d949be
Juergen Fesslmeier <jfesslmeier@gmail.com>
2015-10-15 18:44:56 -0700

    Disabling autoscaler

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 require 'sidekiq'
-require 'autoscaler/sidekiq'
-require 'autoscaler/heroku_scaler'
-require 'autoscaler/stub_scaler'
+# require 'autoscaler/sidekiq'
+# require 'autoscaler/heroku_scaler'
+# require 'autoscaler/stub_scaler'
 
 # You might need increase the pool size of your AR database, see DB_POOL.
 
@@ -10,23 +10,23 @@ Sidekiq.configure_client do |config|
   # Only change this value if you query the Redis database more than just for adding tasks to the Sidekiq queue.
   config.redis = {size: 1, url: ENV["REDISTOGO_URL"] || "redis://localhost:6379/", namespace: 'sidekiq'}
   config.logger = nil
-  config.client_middleware do |chain|
-    if Rails.env.production?
-      chain.add Autoscaler::Sidekiq::Client, 'default' => Autoscaler::HerokuScaler.new
-    else
-      chain.add Autoscaler::Sidekiq::Client, 'default' => Autoscaler::StubScaler.new
-    end
-  end
+  # config.client_middleware do |chain|
+  #   if Rails.env.production?
+  #     chain.add Autoscaler::Sidekiq::Client, 'default' => Autoscaler::HerokuScaler.new
+  #   else
+  #     chain.add Autoscaler::Sidekiq::Client, 'default' => Autoscaler::StubScaler.new
+  #   end
+  # end
 end
 
 Sidekiq.configure_server do |config|
   config.redis = {url: ENV["REDISTOGO_URL"] || "redis://localhost:6379/", namespace: 'sidekiq'}
 
-  config.server_middleware do |chain|
-    if Rails.env.production?
-      chain.add(Autoscaler::Sidekiq::Server, Autoscaler::HerokuScaler.new, 60) # 60 second timeout
-    else
-      chain.add(Autoscaler::Sidekiq::Server, Autoscaler::StubScaler.new, 60) # 60 second timeout
-    end
-  end
+  # config.server_middleware do |chain|
+  #   if Rails.env.production?
+  #     chain.add(Autoscaler::Sidekiq::Server, Autoscaler::HerokuScaler.new, 60) # 60 second timeout
+  #   else
+  #     chain.add(Autoscaler::Sidekiq::Server, Autoscaler::StubScaler.new, 60) # 60 second timeout
+  #   end
+  # end
 end
