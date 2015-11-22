0a0593cca53dfeeda0ed2709ac2f293461876a58
Juergen Fesslmeier <jfesslmeier@gmail.com>
2015-11-22 15:54:46 -0800

    Remove Paulo Coelho's quote

--- app/helpers/web/application_helper.rb
+++ app/helpers/web/application_helper.rb
@@ -2,7 +2,6 @@ module Web::ApplicationHelper
   MARKDOWN_OPTIONS = {:autolink => true, :space_after_headers => true, :fenced_code_blocks => true}
   FAMOUS_QUOTES = [
     ["Shoot for the moon. Even if you miss, you'll land among the stars.", "Les Brown"],
-    ["It's the possibility of having a dream come true that makes life interesting.", "Paulo Coelho"],
     ["Ignore people who say it can't be done.", "Elaine Rideout"],
     ["Don't let anyone steal your dream. It's your dream, not theirs.", "Dan Zadra"],
     ["At least once a day, allow yourself the freedom to think and dream for yourself.", "Albert Einstein"],
