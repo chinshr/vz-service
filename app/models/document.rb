0269109b9dfa189208bddfb9689e01e2a52af9d2
Juergen Fesslmeier <jfesslmeier@gmail.com>
2016-02-23 13:39:51 -0800

    Include :track in documents eager load

--- app/models/document.rb
+++ app/models/document.rb
@@ -123,7 +123,7 @@ class Document < ActiveRecord::Base
     end
     where("documents.slug = ? OR documents.slug_id = ? OR documents.uid = ? OR documents.id = ?", params[:id], params[:id], params[:id], param_document_id)
   }
-  scope :eager_load_associations, -> { eager_load({:images => :image_format}) }
+  scope :eager_load_associations, -> { eager_load([:track, {:images => :image_format}]) }
 
   aasm column: 'aasm_state' do
     state :unpublished, initial: true
