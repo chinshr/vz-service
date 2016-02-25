2bb62a4fc758e29e4c0615fc9986b2403928ba72
Juergen Fesslmeier <jfesslmeier@gmail.com>
2016-02-24 17:34:44 -0800

    Change from JOIN to INNER JOIN in best chunks

--- app/models/document.rb
+++ app/models/document.rb
@@ -228,7 +228,7 @@ class Document < ActiveRecord::Base
   def best_chunks
     chunks.
       joins("INNER JOIN segments ys ON ys.chunk_id = documents.id AND ys.type IN ('Segment::ChunkSegment')").
-      joins("JOIN (SELECT ps.position AS position, MAX(score) AS max_score FROM documents p INNER JOIN segments ps ON ps.chunk_id = p.id AND ps.document_id = #{self.id} AND ps.type IN ('Segment::ChunkSegment') GROUP BY ps.position) y ON y.position = ys.position AND y.max_score = documents.score").
+      joins("INNER JOIN (SELECT ps.position AS position, MAX(score) AS max_score FROM documents p INNER JOIN segments ps ON ps.chunk_id = p.id AND ps.document_id = #{self.id} AND ps.type IN ('Segment::ChunkSegment') GROUP BY ps.position) y ON y.position = ys.position AND y.max_score = documents.score").
       order("ys.position")
   end
 
