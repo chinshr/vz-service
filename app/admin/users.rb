fc54b9946f4cc4f3550f4df5a1c6e202a3356ba2
Juergen Fesslmeier <jfesslmeier@gmail.com>
2016-05-09 23:16:56 -0700

    Login as action in users controller

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -27,4 +27,22 @@ ActiveAdmin.register User do
     column :created_at
     actions
   end
+
+  show do |user|
+    attributes_table do
+      User.column_names.each do |column|
+        row column
+      end
+      row :login_as do
+        link_to "#{user.name}", login_as_admin_user_path(user.id), :target => '_blank', :class => "button"
+      end
+    end
+  end
+
+  # Allows admins to login as a user
+  member_action :login_as, :method => :get do
+    user = User.find(params[:id])
+    sign_in(user, bypass: true)
+    redirect_to web_dashboard_path
+  end
 end
