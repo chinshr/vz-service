014a1d1d12e2ccee195fdd55597ab8ca3155e85c
Juergen Fesslmeier <jfesslmeier@gmail.com>
2016-10-09 12:01:22 -0700

    Add policy_class to chunk, ingest, track

--- app/models/chunk.rb
+++ app/models/chunk.rb
@@ -89,6 +89,10 @@ class Chunk < ::Document
     end
     alias_method_chain :new, :cast
 
+    def policy_class
+      ChunkPolicy
+    end
+
     def class_names_for(params)
       Array.wrap(params).map {|p| class_name_for(p)}.reject(&:blank?)
     end
