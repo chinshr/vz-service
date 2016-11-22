2798dfcc086456783bef994b22545ea73463f3e0
Juergen Fesslmeier <jfesslmeier@gmail.com>
2016-11-22 15:30:25 -0800

    Change cookie session name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Voyzes::Application.config.session_store :cookie_store, key: '_qscribe_session'
+Voyzes::Application.config.session_store :cookie_store, key: '_voyzes_session'
