89eea07035f66e8d54ba0060679137627d195220
Juergen Fesslmeier <jfesslmeier@gmail.com>
2016-12-28 08:38:13 -0800

    Change registration validator to ILIKE

--- lib/registration_validator.rb
+++ lib/registration_validator.rb
@@ -1,6 +1,6 @@
 class RegistrationValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless ::Registration.accepted.where("registrations.email LIKE ?", value).any?
+    unless ::Registration.accepted.where("registrations.email ILIKE ?", value).any?
       record.errors.add(attribute, :register_email, options.merge(value: value))
     end
   end
