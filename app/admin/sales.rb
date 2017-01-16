0ceff5f1cbb3344ebb1dceb30b58d23bfe7f1ebc
Juergen Fesslmeier <jfesslmeier@gmail.com>
2017-01-16 13:07:56 -0800

    Improve sales/subscription admin scaffolds

--- app/admin/sales.rb
+++ app/admin/sales.rb
@@ -1,5 +1,35 @@
 ActiveAdmin.register Payola::Sale, as: "Sale" do
   menu label: "Sales", parent: "Plans"
 
+  actions :all, :except => [:new, :edit, :destroy]
+
+  filter :email
+  filter :guid
+  filter :state
+  filter :card_last4
+  filter :card_type
   filter :created_at
+
+  scope :all
+  scope :pending
+  scope :processing
+  scope :finished
+  scope :errored
+  scope :refunded
+
+  index do
+    column :id do |resource|
+      link_to(resource.id, resource_path(resource))
+    end
+    column :email
+    column :guid
+    column :amount
+    column :product_type
+    column :state
+    column :stripe_token
+    column :card_type
+    column :card_last4
+
+    actions
+  end
 end
--- app/admin/subscriptions.rb
+++ app/admin/subscriptions.rb
@@ -1,5 +1,38 @@
 ActiveAdmin.register Payola::Subscription, as: "Subscription" do
   menu label: "Subscriptions", parent: "Plans"
 
+  actions :all, :except => [:new, :edit, :destroy]
+
   filter :created_at
+
+  scope :all
+  scope :pending
+  scope :processing
+  scope :active
+  scope :canceled
+  scope :errored
+
+  index do
+    column :id do |resource|
+      link_to(resource.id, "subscriptions/#{resource.id}")
+    end
+    column :email
+    column :guid
+    column :amount do |resource|
+      number_to_currency(resource.amount / 100.0, {unit: "$"})
+    end
+    column :state
+    column :stripe_status
+    column :stripe_token
+    column :card_type
+    column :card_last4
+
+    # actions
+    column do |resource|
+      links = ""
+      links += link_to I18n.t('active_admin.view'), "subscriptions/#{resource.id}", :class => "member_link view_link"
+      links.html_safe
+    end
+  end
+
 end
