e36e8b3654fb821e19e3fde606b18cd647f7e4c0
Juergen Fesslmeier <jfesslmeier@gmail.com>
2017-01-18 20:43:00 -0800

    Add gallery (#109)
      * add styles
      * add gallery slides
      * remove xmargin styles

--- app/assets/javascripts/diecisiete.js
+++ app/assets/javascripts/diecisiete.js
@@ -19,9 +19,99 @@
 //= require lib/rails.validations.bootstrap
 //= require lib/detect_timezone
 //= require lib/siriwave
+//= require lib/jquery.easing
 //= require helpers/common
 
+(function() {
+  function Gallery(opts) {
+    this.selector = opts.selector || "#gallery";
+    this.delay = opts.delay || 1400;
+    this.interval = opts.interval || 3000;
+    this.slides = opts.slides || 3;
+
+    this.didCarouselStart = false;
+    this.didUserStartedManually = false;
+    this.currentSlide = 1;
+    this.homeGalleryTimeout = false;
+    this.homeGalleryInterval = false;
+
+    this.bind();
+  }
+
+  Gallery.prototype.bind = function() {
+    var _this = this;
+    $(_this.selector).on("click", ".item, .dot-off", function() {
+      _this.didUserStartedManually = true;
+      _this.stop();
+      _this.changeSlide($(this));
+    });
+  }
+
+  Gallery.prototype.start = function() {
+    var _this = this;
+    this.didCarouselStart = true;
+
+    this.homeGalleryTimeout = setTimeout(function() {
+      _this.changeSlide(2);
+      this.homeGalleryInterval = setInterval(function() {
+        var nextAutomaticSlide;
+        if (_this.currentSlide < $(_this.selector + " .slide").length) {
+          nextAutomaticSlide = _this.currentSlide + 1;
+        } else {
+          nextAutomaticSlide = 1;
+        }
+        _this.changeSlide(nextAutomaticSlide);
+      }, _this.interval);
+    }, _this.delay);
+  }
+
+  Gallery.prototype.stop = function() {
+    clearTimeout(this.homeGalleryTimeout);
+    clearInterval(this.homeGalleryInterval);
+  }
+
+  Gallery.prototype.changeSlide = function(clickedObject) {
+    var newSlideNumber, selectorNewPosition;
+    if ($(clickedObject).attr("class") == "item" || $(clickedObject).attr("class") == "dot-off") {
+      newSlideNumber = $(clickedObject).attr("id").slice(-1);
+    } else {
+      newSlideNumber = clickedObject;
+    }
+
+    $("#home-gallery-item-" + this.currentSlide).attr("class", "item");
+    $("#home-gallery-item-" + newSlideNumber).attr("class", "item-selected");
+    $("#home-gallery-dot-" + this.currentSlide).attr("class", "dot-off");
+    $("#home-gallery-dot-" + newSlideNumber).attr("class", "dot-on");
+    selectorNewPosition = 25 * (newSlideNumber - 1) + "%";
+    $(this.selector + " .selector").animate({"left": selectorNewPosition}, 400, "easeOutCubic");
+
+    $("#slide-" + this.currentSlide).fadeOut(200, function() {
+      $("#slide-" + this.currentSlide).hide();
+      $("#slide-" + newSlideNumber + " h2").hide();
+      $("#slide-" + newSlideNumber + " p").hide();
+      $("#slide-" + newSlideNumber).show();
+      $("#slide-" + newSlideNumber + " img").css({"top":30, "opacity":0});
+      $("#slide-" + newSlideNumber + " img").delay(200).animate({"top":0, "opacity":1}, 350, "easeOutCubic");
+      $("#slide-" + newSlideNumber + " h2").delay(200).fadeIn(350, "easeOutCubic");
+      if (newSlideNumber == 3) {
+        $("#slide-" + newSlideNumber + " p").delay(200).fadeIn(350, "easeOutCubic");
+      }
+    });
+    this.currentSlide = newSlideNumber;
+    return this.currentSlide;
+  }
+
+  window.Gallery = Gallery;
+})();
+
 $(function() {
+  var gallery = new Gallery({
+    interval: 1400,
+    interval: 3800,
+    slides: 3
+  });
+  gallery.start();
+
   var siriWave = new SiriWave({
     container: document.getElementById('siri-wave'),
     style: "ios9",
