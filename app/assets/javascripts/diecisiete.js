f6054501ba043061e0dbef3798b27b95d0f499a3
Juergen Fesslmeier <jfesslmeier@gmail.com>
2017-01-24 22:27:04 -0800

    Change typed speed, fix copy text (#115)
      * add sidenav
      * making slider menu slide from right side
      * work in progress on burger animation
      * fix burger menu

--- app/assets/javascripts/diecisiete.js
+++ app/assets/javascripts/diecisiete.js
@@ -162,10 +162,74 @@
   window.ScrollHeader = ScrollHeader;
 })();
 
+/* SlideMenu */
+
+(function() {
+  function SlideMenu(opts) {
+    this.toggler = opts.toggler || '.navbar-toggle';
+    this.pagewrapper = opts.pagewrapper || '#content';
+    this.navigationWrapper = opts.navigationWrapper || '.navbar-header';
+    this.menuWidth = opts.menuWidth || '100%'; // the menu inside the slide menu itself
+    this.menuNeg = opts.menuNeg || '-100%';
+    this.slideWidth = opts.slideWidth || '80%';
+    this.slideNeg = opts.slideNeg || '-80%';
+  }
+
+  SlideMenu.prototype.start = function() {
+    var _this = this;
+
+    $('header.page-header .container').append($('<div id="navbar-height-col"></div>'));
+
+    $("header.page-header").on("click", _this.toggler, function(e) {
+      var selected = $(this).hasClass('slide-active');
+
+      $('#slidemenu').stop().animate({
+        right: selected ? _this.menuNeg : '0px'
+      });
+
+      $('#navbar-height-col').stop().animate({
+        right: selected ? _this.slideNeg : '0px'
+      });
+
+      $(_this.pagewrapper).stop().animate({
+        right: selected ? '0px' : _this.slideWidth
+      });
+
+      $(_this.navigationWrapper).stop().animate({
+        right: selected ? '0px' : _this.slideWidth
+      });
+
+      $(this).toggleClass('slide-active', !selected);
+      $('#slidemenu').toggleClass('slide-active');
+
+      $('#content, header.page-header, body, .navbar-header').toggleClass('slide-active');
+    });
+
+    var selected = '#slidemenu, #content, body, .navbar, .navbar-header';
+
+    $(window).on("resize", function () {
+      if ($(window).width() > 767 && $('.navbar-toggle').is(':hidden')) {
+        $(selected).removeClass('slide-active');
+      }
+    });
+  }
+
+  window.SlideMenu = SlideMenu;
+})();
+
 $(function() {
   // scroll header
   var scrollHeader = new ScrollHeader({});
   scrollHeader.start();
+
+  // slide menu
+  var slideMenu = new SlideMenu({
+    menuWidth: "245px",
+    menuNeg: "-245px",
+    slideWidth: "245px",
+    slideNeg: "-245px"
+  });
+  slideMenu.start();
 
   // gallery
   var gallery = new Gallery({
@@ -196,10 +260,10 @@ $(function() {
     //strings: ["Your conversations <br/>deserve a place to be found", "Your conversations <br/>are safe with us"],
     stringsElement: $('#typed-strings'),
     cursorChar: "|",
-    typeSpeed: 25,
-    backSpeed: 10,
-    backDelay: 1000,
-    startDelay: 200,
+    typeSpeed: 0,
+    backSpeed: 0,
+    backDelay: 3500,
+    startDelay: 6000,
     loop: true
   });
 
