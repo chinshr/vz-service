b25dffd50d1e7f2f376ed289fa41f5f01f7bc032
Juergen Fesslmeier <jfesslmeier@gmail.com>
2017-01-25 16:57:20 -0800

    Integrate particles.js (#116)
      * edit typed benefits
      * custom bullet list
      * benefits bullets completed
      * blur parallax image
      * add enhanced image loader
      * fix parallax out of bounds
      * fix image credits
      * fixing contact button

--- app/assets/javascripts/diecisiete.js
+++ app/assets/javascripts/diecisiete.js
@@ -22,6 +22,7 @@
 //= require lib/jquery.easing
 //= require lib/bootstrap.youtubepopup
 //= require lib/typed
+//= require lib/particles
 //= require helpers/common
 
 /* Gallery */
@@ -217,7 +218,101 @@
   window.SlideMenu = SlideMenu;
 })();
 
+/* EnhanceImage */
+// https://css-tricks.com/the-blur-up-technique-for-loading-background-images/
+(function() {
+  function EnhanceImage(opts) {
+    opts = opts || {};
+    this.imgClass = opts.imgClass || '.image';
+    this.imgEnhancedClass = opts.imgEnhancedClass || '.enhanced-image';
+    this.style();
+    this.bind();
+  }
+
+  EnhanceImage.prototype.style = function() {
+    var imageSrc = $(this.imgClass).data("image-src");
+    var style = "<style type='text/css'>" +
+      this.imgEnhancedClass + "{ " +
+        "background-image: url(" + imageSrc + ") !important;" +
+        "animation: sharpen .5s both !important;" +
+      "}" +
+      "</style>";
+    $(style).appendTo("head");
+  };
+
+  EnhanceImage.prototype.bind = function() {
+    var _this = this;
+
+    window.onload = function loadStuff() {
+      var win, doc, img, header, enhancedClass;
+
+      // Quit early if older browser (e.g. IE 8).
+      if (!('addEventListener' in window)) {
+        return;
+      }
+
+      win = window;
+      doc = win.document;
+      img = new Image();
+      header = doc.querySelector(_this.imgClass);
+      enhancedClass = _this.imgEnhancedClass
+
+      // Rather convoluted, but parses out the first mention of a background
+      // image url for the enhanced header, even if the style is not applied.
+      var bigSrc = (function () {
+        // Find all of the CssRule objects inside the inline stylesheet
+        var styles = doc.querySelector('style').sheet.cssRules;
+        // Fetch the background-image declaration...
+        var bgDecl = (function () {
+          // ...via a self-executing function, where a loop is run
+          var bgStyle, i, l = styles.length;
+          for (i = 0; i < l; i++) {
+            // ...checking if the rule is the one targeting the
+            // enhanced header.
+            if (styles[i].selectorText &&
+                styles[i].selectorText === enhancedClass) {
+              // If so, set bgDecl to the entire background-image
+              // value of that rule
+              bgStyle = styles[i].style.backgroundImage;
+              // ...and break the loop.
+              break;
+            }
+          }
+          // ...and return that text.
+          return bgStyle;
+        }());
+        // Finally, return a match for the URL inside the background-image
+        // by using a fancy regex I Googled up, as long as the bgDecl
+        // variable is assigned at all.
+        return bgDecl && bgDecl.match(/(?:\(['|"]?)(.*?)(?:['|"]?\))/)[1];
+      }());
+
+      // Assign an onLoad handler to the dummy image *before* assigning the src
+      img.onload = function () {
+        if (enhancedClass.slice(0, 1) === ".") {
+          enhancedClass = enhancedClass.slice(1, 99);
+        }
+
+        header.className += ' ' + enhancedClass;
+      };
+      // Finally, trigger the whole preloading chain by giving the dummy
+      // image its source.
+      if (bigSrc) {
+        img.src = bigSrc;
+      }
+    };
+  };
+
+  window.EnhanceImage = EnhanceImage;
+})();
+
 $(function() {
+  // enhance images
+  var mainImage = new EnhanceImage({
+    imgClass: ".background-image",
+    imgEnhancedClass: ".enhanced-image"
+  }).bind();
+
   // scroll header
   var scrollHeader = new ScrollHeader({});
   scrollHeader.start();
@@ -289,14 +384,17 @@ $(function() {
   });
 
   // parallax
-  var $window = $(window)
-  $window.scroll(function() {
-    var yPos = -($window.scrollTop() / $('#parallax').data('speed') - 180);
-    var coords = '50%' + yPos + 'px';
-    $('#parallax').css({
-      backgroundPosition: coords
+  if ($('.parallax').length > 0) {
+    var $window = $(window),
+      $parallax = $('.parallax');
+    $window.scroll(function() {
+      var yPos = -($window.scrollTop() / ($parallax.data('speed') || 8) - 180);
+      var coords = '50%' + yPos + 'px';
+      $parallax.css({
+        backgroundPosition: coords
+      });
     });
-  });
+  }
 
   // form
   $("#contact .button-contact-us").on('click', function() {
@@ -313,5 +411,9 @@ $(function() {
       $("#cookie-law-bar").hide();
     });
   }
+
+  // particles
+  particlesJS.load('particles', 'assets/config/benefits-particles.json', function() {
+  });
 
 });
