7afe6d919393f04d0f049b4a027c400ff523f431
Juergen Fesslmeier <jfesslmeier@gmail.com>
2017-01-29 15:48:12 -0800

    Integrate Drift

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
 Rails.application.config.assets.precompile += %w( lib/html5shiv.js lib/respond.min.js )
 Rails.application.config.assets.precompile += %w( beachstrap.scss beachstrap.js )
-Rails.application.config.assets.precompile += %w( diecisiete.scss diecisiete.js )
+Rails.application.config.assets.precompile += %w( diecisiete.scss diecisiete.js web/drift.js )
