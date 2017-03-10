d68d20058eeaa753776465b9adff84bc0228276b
Juergen Fesslmeier <jfesslmeier@gmail.com>
2017-03-09 18:44:29 -0800

    Add fonts types to assets precompile pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
 Rails.application.config.assets.precompile += %w( lib/html5shiv.js lib/respond.min.js )
 Rails.application.config.assets.precompile += %w( beachstrap.scss beachstrap.js )
 Rails.application.config.assets.precompile += %w( diecisiete.scss diecisiete.js web/drift.js )
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
