a85e07a531f7453655e1b2d9ffc61a6c90fce7e5
Juergen Fesslmeier <jfesslmeier@gmail.com>
2017-05-05 09:39:34 -0700

    Adding pack ingest pruning

--- app/jobs/ingest/prune_job.rb
+++ app/jobs/ingest/prune_job.rb
@@ -4,6 +4,7 @@ class Ingest::PruneJob < ActiveJob::Base
   def perform
     fail_stale_starting_ingests
     process_stale_ingests_in_between_states
+    stop_stale_running_ingests
     delete_removed_ingests
   end
 
@@ -24,6 +25,12 @@ class Ingest::PruneJob < ActiveJob::Base
     # re-process 'in between' states, e.g. :starting, etc.
     Ingest.any_of_status([Ingest::STATE_REMOVING, Ingest::STATE_RESETTING, Ingest::STATE_STOPPING]).is_terminate(true).find_each do |ingest|
       Ingest::ProcessJob.perform_later(ingest.id)
+    end
+  end
+
+  def stop_stale_running_ingests
+    Ingest.started.is_busy(false).is_terminate(false).where("ingests.created_at < ?", Time.zone.now - 2.hour).find_each do |ingest|
+      ingest.stop!
     end
   end
 
